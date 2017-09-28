e37480d9a242c974d82b4b75da2a84d6264f2648
DhritiShikhar <dshikhar@redhat.com>
2017-09-28 11:57:40 +0530

    linear search

---
+++ linearSearch.go
@@ -0,0 +1,24 @@
+// Linear Search Algorithm
+
+package main
+
+import "fmt"
+
+func linearsearch(n int, items []int) (bool, int) {
+	for i := 0; i < len(items); i++ {
+		if n == items[i] {
+			return true, i + 1
+		}
+	}
+	return false, 0
+}
+
+func main() {
+	items := []int{6, 2, 9, 7, 3, 1, 8, 10, 4, 5}
+	presence, position := linearsearch(10, items)
+	if !presence {
+		fmt.Println("The number you searched for is not present")
+		return
+	}
+	fmt.Printf("The number is present at location %v\n", position)
+}
